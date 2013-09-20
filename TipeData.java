06085c9424e8f1cca6cb3285670296da8b59f619
Zulsyika Nurfaizah <zulsyika.nurfaizah@gmail.com>
2013-09-20 11:38:50 +0700

    2

---
+++ TipeData.java
@@ -0,0 +1,22 @@
+class TipeData
+{
+	private String nama;
+	public static void main(String[] args)
+	{
+		TipeData isi = new TipeData();
+		isi.setNama("Zulsyika Nurfaizah");
+		byte a = 100;
+		short b = 100;
+		
+		System.out.println("Menampilkan nama = "+isi.getNama());
+		System.out.println("Hasil dari a + b = "+(a+b));
+	}
+	public void setNama(String aNama)
+	{
+		this.nama = aNama;
+	}
+	public String getNama()
+	{
+		return this.nama;
+	}
+}
