46e42cd3b9fa4df44aa709ef6f12304f0af82930
Zulsyika Nurfaizah <zulsyika.nurfaizah@gmail.com>
2013-09-20 11:36:02 +0700

    1

---
+++ Person.java
@@ -0,0 +1,10 @@
+class Person{
+	public static void main(String[] syika){
+		int x = 10;
+		char y;
+		y = 'A';
+		
+		System.out.println("Tampilkan variable x "+x);
+		System.out.println("Tampilkan variable y "+y);
+	}
+}	
